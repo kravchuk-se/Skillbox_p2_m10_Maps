b9e7743df77bc29a3573b97cda4a34483bed2cc5
Sergey Kravchuk <kravchuk@icloud.com>
2020-02-03 21:41:15 +0300

    Initial commit

---
+++ Skillbox_m10 (Maps)/PlacesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PlacesViewController.swift
+//  Skillbox_m10 (Maps)
+//
+//  Created by Kravchuk Sergey on 03.02.2020.
+//  Copyright © 2020 Kravchuk Sergey. All rights reserved.
+//
+
+import UIKit
+
+class PlacesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
